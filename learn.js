b63634d54fe8f5f58750f8deda52ffcbf082fbcf
Matthew Dreier <43585853+matthewsdreier@users.noreply.github.com>
2023-05-19 23:03:07 -0400

    Add precise values for examples 1-3 in learn.html

--- learn.js
+++ learn.js
@@ -3,7 +3,7 @@ const graph2 = document.querySelector(".graph2");
 const graph3 = document.querySelector(".graph3");
 const graph4 = document.querySelector(".graph4");
 
-makeGraph(box = graph1, sn = 85, sp = 97);
-makeGraph(box = graph2, sn = 96, sp = 27);
-makeGraph(box = graph3, sn = 60, sp = 90);
-makeGraph(box = graph4, sn = 55, sp = 70);
+makeGraph(box = graph1, sn = 83, sp = 96);
+makeGraph(box = graph2, sn = 97.4, sp = 21.9);
+makeGraph(box = graph3, sn = 11.1, sp = 97.4);
+makeGraph(box = graph4, sn = 11.1, sp = 97.4);
