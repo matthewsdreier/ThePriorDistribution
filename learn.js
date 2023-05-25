6d7fac3296c6b784bd83d047208d4e635bc2a28b
Matthew Dreier <43585853+matthewsdreier@users.noreply.github.com>
2023-05-24 22:05:54 -0400

    Complete examples in learn.html
    Add example of TTE
    Add example of pleural effusion

--- learn.js
+++ learn.js
@@ -5,5 +5,5 @@ const graph4 = document.querySelector(".graph4");
 
 makeGraph(box = graph1, sn = 83, sp = 96);
 makeGraph(box = graph2, sn = 97.4, sp = 21.9);
-makeGraph(box = graph3, sn = 11.1, sp = 97.4);
-makeGraph(box = graph4, sn = 11.1, sp = 97.4);
+makeGraph(box = graph3, sn = 41, sp = 91);
+makeGraph(box = graph4, sn = 35, sp = 70);
